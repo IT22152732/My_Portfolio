d15ce84011c390a183198db21435adc9d4655389
Hasara Samadhi Herath <it22152732@my.sliit.lk>
2024-06-15 22:34:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Interactive Media UG", "UI/UX Developer", "Mobile Application Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
